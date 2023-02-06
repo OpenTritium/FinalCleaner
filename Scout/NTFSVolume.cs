76223f40ef6d8d098d3e7c42b0c5fd1f2b74e339
TritiumQin <tritium404@hotmail.com>
2023-02-06 22:03:18 +0800

    通过分配非托管内存解决报错

--- Scout/NTFSVolume.cs
+++ Scout/NTFSVolume.cs
@@ -5,17 +5,19 @@ using static PInvoke.Kernel32.CreateFileFlags;
 using static PInvoke.Kernel32.CreationDisposition;
 using static PInvoke.Kernel32.FileShare;
 
+// 记得处理句柄，还有未托管内存！！！
+
 namespace Scout
 {
-    internal sealed class NTFSVolume : Volume, IRecorder
+    internal sealed class NtfsVolume : Volume, IRecorder
     {
         private readonly SafeObjectHandle _volumeHandle;
-        private USN_JOURNAL_DATA _USNJournalData;
+        private UsnJournalData _USNJournalData;
 
         /// <summary>
         /// IO 控制码，决定了 DeviceIoControl 的行为。
         /// </summary>
-        private enum IoControlCode : int
+        private enum IoControlCode
         {
             FSCTL_ENUM_USN_DATA = 0x900b3,
             FSCTL_CREATE_USN_JOURNAL = 0x900e7,
@@ -29,19 +31,19 @@ namespace Scout
         /// 每个卷都有一个这样的数据库。
         /// </summary>
         [StructLayout(LayoutKind.Sequential)]
-        private struct CREATE_USN_JOURNAL_DATA
+        private struct CreateUsnJournalData
         {
             public ulong MaximumSize;  // 为日志缓冲区分配的最大空间。
             public ulong AllocationDelta;  // 如果超过了，就删掉前面指定字节数量的记录，这样后面添加的记录会被写在追加的新空间。
         }
 
         /// <summary>
-        /// 由于 WIN8 以上系统使用 USN_JOURNAL_DATA_V1
+        /// 由于 WIN8 以上系统使用 UsnJournalData_V1
         /// 此处使用 V1 版本（本来就没考虑过兼容 WIN10 以下的版本）
         /// USN 是更新序列号，是自增的但不连续，越小代表代表的事件发生的越早
         /// </summary>
         [StructLayout(LayoutKind.Sequential)]
-        private struct USN_JOURNAL_DATA
+        private struct UsnJournalData
         {
             public ulong UsnJournalID;  // 当前日志标识符，不等同 USN
             public ulong FirstUsn;  // 第一条 USN
@@ -56,11 +58,11 @@ namespace Scout
 
         /// <summary>
         /// 用于输入 DeviceIoControl，决定了被枚举的 USN 数据的属性
-        /// 由于 WIN8 以上系统使用 MFT_ENUM_DATA_V1
+        /// 由于 WIN8 以上系统使用 MftEnumData_V1
         /// 此处使用 V1 版本（本来就没考虑过兼容 WIN10 以下的版本）
         /// </summary>
         [StructLayout(LayoutKind.Sequential)]
-        private struct MFT_ENUM_DATA
+        private struct MftEnumData
         {
             public ulong StartFileReferenceNumber;  //第一次设置的时候为 0，到下一页缓存的时候重新获取
             public ulong LowUsn; // USN 值的上限
@@ -69,16 +71,11 @@ namespace Scout
             public ushort MaxMajorVersion;  // 最大日志版本，这个可能造成返回版本不同的日志的情况
         }
 
-        [StructLayout(LayoutKind.Explicit, Size = 8)]
-        private struct LARGE_INTEGER
-        {
-            [FieldOffset(0)] public Int64 QuadPart;
-            [FieldOffset(0)] public UInt32 LowPart;
-            [FieldOffset(4)] public Int32 HighPart;
-        }
-
+        /// <summary>
+        /// UsnRecord_V2
+        /// </summary>
         [StructLayout(LayoutKind.Sequential)]
-        private struct USN_RECORD
+        private struct UsnRecord
         {
             public uint RecordLength;
             public ushort MajorVersion;
@@ -86,17 +83,27 @@ namespace Scout
             public ulong FileReferenceNumber;
             public ulong ParentFileReferenceNumber;
             public ulong Usn;
-            private LARGE_INTEGER TimeStamp;
-            private uint Reason;
-            private uint SourceInfo;
-            private uint SecurityId;
-            private uint FileAttributes;
-            private ushort FileNameLength;
-            private ushort FileNameOffset;
-            private char[] FileName;
+            public ulong TimeStamp;
+            public uint Reason;
+            public uint SourceInfo;
+            public uint SecurityId;
+            public uint FileAttributes;
+            public ushort FileNameLength;
+            public ushort FileNameOffset;
+            public char[] FileName;  // 这里的 char 是 Unicode，但是系统提供的应该是 ANSI 的
         }
 
-        internal NTFSVolume(DriveInfo driveInfo) : base(driveInfo)
+        /// <summary>
+        /// 我们需要一种数据结构，描述当前文件节点信息
+        /// </summary>
+        private struct FileInfoNode
+        {
+            public ulong parentRef;  // 上级目录文件索引号
+            public string? fileName;
+            public ulong timeStamp;
+        }
+
+        internal NtfsVolume(DriveInfo driveInfo) : base(driveInfo)
         {
             _USNJournalData = new();
             _volumeHandle = CreateFile(
@@ -114,7 +121,7 @@ namespace Scout
 
         internal bool CreateUSNJournal()
         {
-            CREATE_USN_JOURNAL_DATA CUJD = new();  // 初始化为 0
+            CreateUsnJournalData CUJD = new();  // 初始化为 0
             bool isCreated;
             unsafe
             {
@@ -122,7 +129,7 @@ namespace Scout
                     _volumeHandle,
                     (int)IoControlCode.FSCTL_CREATE_USN_JOURNAL,
                     &CUJD,
-                    sizeof(CREATE_USN_JOURNAL_DATA),
+                    sizeof(CreateUsnJournalData),
                     null,
                     0,
                     out _,  // lpOverlapped 为空指针时 pBytesreturned 不能为空
@@ -133,7 +140,7 @@ namespace Scout
 
         private bool QueryUSNJournal()
         {
-            var UJD = default(USN_JOURNAL_DATA);
+            var UJD = default(UsnJournalData);
             bool isCreated;
             unsafe
             {
@@ -143,7 +150,7 @@ namespace Scout
                         null,
                         0,
                         &UJD,
-                        sizeof(USN_JOURNAL_DATA),
+                        sizeof(UsnJournalData),
                         out _,  // lpOverlapped 为空指针时 pBytesreturned 不能为空
                         (OVERLAPPED*)null);
             }
@@ -157,43 +164,56 @@ namespace Scout
 
         private bool EnumUSNData()
         {
-            MFT_ENUM_DATA MFTEnumData = new()
+            MftEnumData mft = new()
             {
                 StartFileReferenceNumber = 0,
                 LowUsn = _USNJournalData.FirstUsn,
                 HighUsn = _USNJournalData.NextUsn,
-                MinMajorVersion = _USNJournalData.MinSupportedMajorVersion,
-                MaxMajorVersion = _USNJournalData.MaxSupportedMajorVersion,
+                MinMajorVersion = 2,
+                MaxMajorVersion = 2,
                 // 把日志版本卡的死死的，后面版本的结构体有变化，先停在最小支持版本
             };
-            const int BUF_LEN = 0x10000;
-            char[] buffer = new char[BUF_LEN];
-            var pBuffer = IntPtr.Parse(buffer);
-            USN_RECORD record = new();
-            unsafe
-            {
-                while (DeviceIoControl(
+
+            IntPtr pMft = Marshal.AllocHGlobal(Marshal.SizeOf(mft));  // 为输入缓存准备非托管内存
+            Marshal.StructureToPtr<MftEnumData>(mft, pMft, true);  // 指向结构体的指针
+            const int bufferSize = 131027;  // 128KB
+            IntPtr pBuffer = Marshal.AllocHGlobal(bufferSize);  // 为输出缓存分配非托管内存
+            Dictionary<ulong, FileInfoNode> FileInfoMap = new();
+            FileInfoNode Node = new();
+
+            while (DeviceIoControl(
                     _volumeHandle,
                     (int)IoControlCode.FSCTL_ENUM_USN_DATA,
-                    &MFTEnumData,
-                    sizeof(MFT_ENUM_DATA),
-                    pBuffer.ToPointer(),
-                    BUF_LEN,
+                    pMft,
+                    Marshal.SizeOf<MftEnumData>(mft),
+                    pBuffer,
+                    bufferSize,
                     out int USNDataSize,
-                    (OVERLAPPED*)null))
+                    IntPtr.Zero))
+            {
+                /* 为访问下条 USN 记录做准备 */
+                var RetBytes = Convert.ToUInt32(USNDataSize);  
+                // 奶奶滴微软的文档什么时候能统一一下，有的写的是 uint 有的写的是 int
+                unsafe
                 {
-                    /* 为访问下条 USN 记录做准备 */
-                    USNDataSize -= sizeof(ulong);  // USN 数据空间减去一条记录的长度
-                    MFTEnumData.StartFileReferenceNumber = *(ulong*)pBuffer;  // 每次调用 FSCTL_ENUM_USN_DATA 检索后续调用的起点作为输出缓冲区中的第一个条目
-                    record = *(USN_RECORD*)(pBuffer+sizeof(ulong));
-                };
+                    RetBytes -= sizeof(ulong);  // USN 数据空间减去一条记录的长度
+                    IntPtr pUsnRecord = new IntPtr(pBuffer.ToInt64());
+                    while (RetBytes > 0)
+                    {
+                        var usnRecord = Marshal.PtrToStructure<UsnRecord>(pUsnRecord);
+                        Node.fileName = usnRecord.FileName.ToString();
+                        Node.parentRef = usnRecord.ParentFileReferenceNumber;
+                        Node.timeStamp = usnRecord.TimeStamp;
+                        FileInfoMap[usnRecord.FileReferenceNumber] = Node;
+                        var recordLen = usnRecord.RecordLength;
+                        RetBytes -= recordLen;
+                        pUsnRecord = IntPtr.Subtract(pUsnRecord, (int)recordLen);  // 读下条 USN 日志
+                    }
+                    mft.StartFileReferenceNumber = (ulong)Marshal.ReadInt64(pBuffer, 0);
+                    // 每次调用 FSCTL_ENUM_USN_DATA 检索后续调用的起点作为输出缓冲区中的第一个条目
+                }
             }
-        }
-
-        private bool GainUSNJournal()
-        {
-            // StartFileReferenceNumber 被初始化为 0
-            MFTEnumData.LowUsn = _USNJournalData.FirstUsn;
+            return true;
         }
 
         public bool GenerateVolumeIndex()
